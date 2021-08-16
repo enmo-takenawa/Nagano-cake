3df7300bcdab08df934df1a1b9e03019b5d04875
nogamishinpei <nogamichiru@gmail.com>
2021-08-16 17:45:41 +0000

    homescontroll

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,10 @@ class HomesController < ApplicationController
     
     
     def top
-        
+        #新着商品4つ表示させるバランスを見て5個とかでもいいかも？
+    @goods = Goods.first(4)
+    #もしもジャンル検索を適用するなら下のコメントアウトの＃消してください
+    #@genre = Genre.all
     end
     
     def about
