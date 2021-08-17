1f027284ab7444441216b830abab6ecf9ef65c87
nogamishinpei <nogamichiru@gmail.com>
2021-08-17 06:01:18 +0000

    shipping_address_model

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,11 +1,9 @@
 class ShippingAddress < ApplicationRecord
     
     #バリデーション、苗字、名前、郵便番号、住所
-    validates :last_name, presence: true
-    validates :first_name, presence: true
-    #郵便番号の設定、ハイフンなし、七桁の数字
-    validates :post_code, presence: true, format: {with: /\A\d{7}\z/}
+    validates :name, presence: true
     validates :address, presence: true
+    validates :tel, presence: true
     
     belongs_to :customer
     
