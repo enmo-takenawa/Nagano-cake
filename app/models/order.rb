1d87c6ba0423b6630acea52ab10316fdf8bdfb78
ch-mei <mei013h0ws@gmail.com>
2021-08-17 08:06:50 +0000

    ordermodel create

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ApplicationRecord
   has_many :customers
   belongs_to :ordered_goods
-  
-  VALID_POSTAL_CODE_REGEX = /^\d{7}$/
-  validates :shipping_postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
+
+  validates :shipping_postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  #郵便番号　7桁（ハイフン無し）
+  validates :shipping_address, presence: true
+  validates :shipping_name, presence: true
+
+
 end
