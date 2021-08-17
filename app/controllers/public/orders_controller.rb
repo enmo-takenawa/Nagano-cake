b28eb2daed9f031dc58c0ac12e390fca5f90c2fc
ch-mei <mei013h0ws@gmail.com>
2021-08-17 12:56:41 +0000

    public/order controller add

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,21 @@
+class Public::OrdersController < ApplicationController
+  
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def confirmation
+  end
+  
+  def success
+  end
+  
+end
