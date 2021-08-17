b1cb0788662f30ffb4177b8abfbba87ff54a7666
ch-mei <mei013h0ws@gmail.com>
2021-08-17 13:33:41 +0000

    admin/order controller action記述

--- db/migrate/20210817063656_create_orders.rb
+++ db/migrate/20210817063656_create_orders.rb
@@ -1,13 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :shipping_postal_code
-      t.string :shipping_address
-      t.string :shipping_name
-      t.integer :shipping_fee
-      t.integer :total_price
-      t.integer :payment_method
-      t.integer :order_status
+      t.string :shipping_postal_code #郵便番号
+      t.string :shipping_address #住所
+      t.string :shipping_name #氏名
+      t.integer :shipping_fee #送料
+      t.integer :total_price #請求金額
+      t.integer :payment_method #支払方法
+      t.integer :order_status #注文ステータス
       
 
       t.timestamps
