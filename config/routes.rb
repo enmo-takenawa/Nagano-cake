14efa330b008fdb12c2c47fcd1ffd42fd8077ea3
Geoglyph000 <nasca19970911@gmail.com>
2021-08-17 02:41:27 +0000

    set customers routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  
+  resources :customers, only: [:edit, :show, :update]
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  resources :shipping_addresses
+  resources :goods, only: [:index, :show]
+  resources :cart_items
+  resources :orders
+  post 'orders/confirmation' => 'orders#confirmation'
+  get 'orders/success' => 'orders#success'
+  
 end
