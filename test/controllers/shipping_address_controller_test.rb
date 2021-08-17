d6ffe22507a0b29505b1b8d30762764ea60b9220
nogamishinpei <nogamichiru@gmail.com>
2021-08-17 04:34:39 +0000

    カート内商品と配送先コントローラー作成

--- test/controllers/shipping_address_controller_test.rb
+++ test/controllers/shipping_address_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ShippingAdressControllerTest < ActionDispatch::IntegrationTest
+class ShippingAddressControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
