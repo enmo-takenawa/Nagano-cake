7346618254648feac643051de5786ec2652d6426
Geoglyph000 <nasca19970911@gmail.com>
2021-08-18 09:52:18 +0000

    create views/customers to devise work, edit sign_in view

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,25 @@ Rails.application.routes.draw do
     sessions: 'admins/sessions'
   } #管理者ログイン画面へのルーティング
 
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'public/devise/sessions'
+  } 
+  
 
-
-  root to: 'homes#top'
-  get 'home/about' => 'homes#about'
-
-  resources :customers, only: [:edit, :show, :update]
-  get 'customers/unsubscribe' => 'customers#unsubscribe'
-  resources :shipping_addresses
-  resources :goods, only: [:index, :show]
-  resources :cart_items
-  delete 'cart_items' => 'cart_items#destroy_all'
-  resources :orders
-  post 'orders/confirmation' => 'orders#confirmation'
-  get 'orders/success' => 'orders#success'
+  scope module: :public do
+    root to: 'homes#top'
+    get 'home/about' => 'homes#about'
+  
+    resources :customers, only: [:edit, :show, :update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    resources :shipping_addresses
+    resources :goods, only: [:index, :show]
+    resources :cart_items
+    delete 'cart_items' => 'cart_items#destroy_all'
+    resources :orders
+    post 'orders/confirmation' => 'orders#confirmation'
+    get 'orders/success' => 'orders#success'
+  end
 
   namespace :admin do
     resources :orders
