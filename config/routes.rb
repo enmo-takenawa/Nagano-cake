366beae7e94db0d219966cf446fce5441f6f1ac2
Geoglyph000 <nasca19970911@gmail.com>
2021-08-18 06:37:18 +0000

    edit views & edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,22 @@ Rails.application.routes.draw do
 
   devise_for :customers
 
+  scope module: :public do
+    root to: 'homes#top'
+    get 'home/about' => 'homes#about'
+  
+    resources :customers, only: [:edit, :show, :update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    resources :shipping_addresses
+    resources :goods, only: [:index, :show]
+    resources :cart_items
+    delete 'cart_items' => 'cart_items#destroy_all'
+    resources :orders
+    post 'orders/confirmation' => 'orders#confirmation'
+    get 'orders/success' => 'orders#success'
+  end
 
-  root to: 'homes#top'
-  get 'home/about' => 'homes#about'
-
-  resources :customers, only: [:edit, :show, :update]
-  get 'customers/unsubscribe' => 'customers#unsubscribe'
-  resources :shipping_addresses
-  resources :goods, only: [:index, :show]
-  resources :cart_items
-  delete 'cart_items' => 'cart_items#destroy_all'
-  resources :orders
-  post 'orders/confirmation' => 'orders#confirmation'
-  get 'orders/success' => 'orders#success'
-
-  namespace :admin do
+  namespace :admins do
     resources :orders
     patch 'ordered_goods/:id' => 'ordered_goods#update'
     resources :goods
