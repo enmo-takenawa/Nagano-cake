498b611c523f46c1d761043a33275f51a5af4e41
Geoglyph000 <nasca19970911@gmail.com>
2021-08-18 12:31:56 +0000

    edit sign_up view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_142826) do
+ActiveRecord::Schema.define(version: 2021_08_18_121754) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2021_08_17_142826) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "first_name"
-    t.string "last_name　"
-    t.string "first_name_kana　"
+    t.string "last_name"
+    t.string "first_name_kana"
     t.string "last_name_kana"
     t.string "postal_code"
     t.string "address"
