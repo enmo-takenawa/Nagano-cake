2db51b848285758776d74232a2ec9155b0efb481
Geoglyph000 <nasca19970911@gmail.com>
2021-08-18 02:49:48 +0000

    create controllers both of admins/public

---
+++ app/controllers/admins/goods_controller.rb
@@ -0,0 +1,2 @@
+class Admins::GoodsController < ApplicationController
+end
---
+++ test/controllers/admins/goods_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admins::GoodsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
