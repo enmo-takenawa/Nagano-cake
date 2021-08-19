fd4c24847cb86356ec5c4b470a42ece6a426ad1e
amaama54 <centerfielder-msk-54@docomo.ne.jp>
2021-08-19 07:18:40 +0000

    admins_goods_edit

--- app/controllers/admins/goods_controller.rb
+++ app/controllers/admins/goods_controller.rb
@@ -14,8 +14,18 @@ class Admins::GoodsController < ApplicationController
   end
 
   def edit
+    @good = Good.find(params[:id])
   end
 
   def update
+    @good = Good.find(params[:id])
+    @good.update(good_params)
+    redirect_to admins_good_path(@good.id)
   end
+  
+  private
+  def good_params
+    params.require(:good).permit(:name, :introduction, :genre_id, :price, :sales_status)
+  end
+
 end
