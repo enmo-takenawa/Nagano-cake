e37553c0bbc92db37d929e03e8f087a5d476d4d1
amaama54 <centerfielder-msk-54@docomo.ne.jp>
2021-08-21 11:37:21 +0000

    [fix] goods,genres/edit

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admins::OrdersController < ApplicationController
   
   def index
-    @ordered_goods =Ordered_goods.all.page(params[:page]).per(10)
+    @ordered_goods =Order.all.page(params[:page]).per(10)
   end
 
   def show
