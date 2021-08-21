ff74bb3aef05a7ad62524c91bb1404e4746f1d9b
ch-mei <mei013h0ws@gmail.com>
2021-08-21 12:30:01 +0000

    cart index 仮作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,30 +1,51 @@
 class Public::CartItemsController < ApplicationController
   def index
-        @customer = Customer.find(params[:id])
-        @goods = Goods.all
+    @cart_items = CartItem.new
+    @cart_items = current_customer.cart_items
   end
-    
+
   def update
-        @cart_item = CartItem.find(params[:id])
-        @cart_item.update(cart_item_params)
-        
+    cart_items = current_customer.cart_items
+    cart_item = cart_items.find_by(good_id: params[:cart_item][:good_id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
-    
+
   def create
-      @cart_item = CartItem.new(cart_item_params)
-      @cart_item.customer_id = current_customer.id
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items
+    unless @cart_item.quantity.nil?
+      @cart_items.each do |cart_item|
+        if cart_item.good_id == @cart_item.good_id
+          new_quantity = cart_item.quantityt + @cart_item.quantity
+          cart_item.update_attribute(:quantity, new_quantity)
+          @cart_item.delete
+        end
+      end
       @cart_item.save
       redirect_to cart_items_path
+    else
+      @good = Good.find(params[:cart_item][:good_id])
+      render ("public/goods/show")
+    end
   end
 
   def destroy
-        cart_item = CartItem.find(params[:id])
-        cart_item.destroy
-        redirect_to cart_item_path
+    cart_item = current_customer.cart_items.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
-    
+
   def destroy_all
-        current_customer.cart_items.destroy_all
-        redirect_to cart_items_path
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to cart_items_path
   end
+  
+  private
+  def car_item_params
+    params.require(:cart_item).permit(:good_id, :quantity)
+  end
+
 end
