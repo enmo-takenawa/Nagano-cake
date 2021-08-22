0da1f152994336dc08d93cb6c398232616543939
ch-mei <mei013h0ws@gmail.com>
2021-08-22 03:22:12 +0000

    cart_item view create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,12 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.new
+   #@cart_items = CartItem.new
     @cart_items = current_customer.cart_items
   end
 
   def update
     cart_items = current_customer.cart_items
-    cart_item = cart_items.find_by(good_id: params[:cart_item][:goods_id])
+    cart_item = cart_items.find_by(good_id: params[:cart_item][:good_id])
     cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
@@ -14,21 +14,9 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_items = current_customer.cart_items
-    unless @cart_item.quantity.nil?
-      @cart_items.each do |cart_item|
-        if cart_item.good_id == @cart_item.good_id
-          new_quantity = cart_item.quantityt + @cart_item.quantity
-          cart_item.update_attribute(:quantity, new_quantity)
-          @cart_item.delete
-        end
-      end
-      @cart_item.save
-      redirect_to cart_items_path
-    else
-      @good = Good.find(params[:cart_item][:goods_id])
-      render ("public/goods/show")
-    end
+    #@cart_items = current_customer.cart_items
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -45,7 +33,7 @@ class Public::CartItemsController < ApplicationController
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:goods_id, :quantity)
+    params.require(:cart_item).permit(:good_id, :quantity)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :shipping_address
   has_many :orders, through: :ordered_goods
-
+  has_many :cart_items
 end
