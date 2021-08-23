5ee92a3b31366f70a17b51cf845c71126b45afae
Geoglyph000 <nasca19970911@gmail.com>
2021-08-23 07:10:56 +0000

    initial commit

--- db/migrate/20210819150359_add_customer_id_to_shipping_addresses.rb
+++ db/migrate/20210819150359_add_customer_id_to_shipping_addresses.rb
@@ -2,8 +2,6 @@ class AddCustomerIdToShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     add_column :shipping_addresses, :customer_id, :integer
     add_column :orders, :customer_id, :integer
-    add_column :cart_items, :customer_id, :integer
-    add_column :cart_items, :quantity, :integer
-    add_column :cart_items, :goods_id, :integer
+    
   end
 end
