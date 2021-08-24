110c2714f7478f8599e5b6a92a302b88ce458364
ch-mei <mei013h0ws@gmail.com>
2021-08-24 03:07:11 +0000

    genre window add

--- app/controllers/public/goods_controller.rb
+++ app/controllers/public/goods_controller.rb
@@ -1,9 +1,12 @@
 class Public::GoodsController < ApplicationController
   def index
+    active_goods = Good.where(sales_status: false)
     @goods = Good.page(params[:page]).per(8).reverse_order
-    
+    @goods_count = active_goods.count
+    @genres = Genre.all
+
   end
-  
+
   def show
     @good = Good.find(params[:id])
     @cart_item = CartItem.new
