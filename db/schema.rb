1a23712850c0762ee1c4321e0db28bea4581e131
Geoglyph000 <nasca19970911@gmail.com>
2021-08-24 07:39:39 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_070303) do
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,9 +43,9 @@ ActiveRecord::Schema.define(version: 2021_08_24_070303) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_deleted", default: true, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -87,7 +85,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_070303) do
     t.integer "shipping_fee"
     t.integer "total_price"
     t.integer "payment_method"
-    t.integer "order_status", default: 0
+    t.integer "order_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
