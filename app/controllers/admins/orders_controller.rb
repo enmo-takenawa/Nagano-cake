9e18f17050cd52fbec087751f72ce01d250c0100
ch-mei <mei013h0ws@gmail.com>
2021-08-25 05:24:33 +0000

    注文履歴詳細画面　プルタグ自動更新

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -8,22 +8,27 @@ class Admins::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     #@ordered_goods = OrderedGood.find(params[:id])
     @order.shipping_fee = 800
+    @array_order_status = Array.new
+    @array_order_status.push(["入金待ち",0],["入金確認中",1],["製作中",2],["発送準備中",3],["発送済み",4])
+    @array_ordered_good_production_status = Array.new
+    @array_ordered_good_production_status.push(["着手不可",0],["制作待ち",1],["製作中",2],["製作完了",3])
   end
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_status: params[:order][:order_status].to_i)
-    if params[:order][:order_status].to_i == 1
-      @order.ordered_goods.update_all(production_status: 1)
-    elsif params[:order][:order_status].to_i == 4
-      @order.ordered_goods.update_all(production_status: 3)
+    if @order.update(order_status: params[:order][:order_status].to_i)
+      if Order.order_statuses[@order.order_status] == 1
+        @order.ordered_goods.each do|f|
+          f.update(production_status: 1)
+        end
+      end
+      redirect_to request.referer
     end
-    redirect_to admins_order_path(@order.id)
   end
 
   private
-  def order_params
-    params.require(:order).permit(:order_status) #permitの中身記述途中
-  end
+  #def order_params
+    #params.require(:order).permit(:order_status) #permitの中身記述途中
+  #end
 
 end
