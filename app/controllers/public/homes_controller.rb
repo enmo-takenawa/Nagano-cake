6b74da7ab012b04ab894fd432917b487b2c12ecf
Geoglyph000 <nasca19970911@gmail.com>
2021-08-25 07:22:12 +0000

    debug new_goods at homes/top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @goods = Good.first(4) #新着商品4つ表示させるバランスを見て5個とかでもいいかも？
+    @goods = Good.order("created_at DESC").limit(4)#新着商品4つ表示させるバランスを見て5個とかでもいいかも？
     #もしもジャンル検索を適用するなら下のコメントアウトの＃消してください
     #@genre = Genre.all
   end
