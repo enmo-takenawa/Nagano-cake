32d092487e6e18611669e7a820df33b9b1124b0b
ch-mei <mei013h0ws@gmail.com>
2021-08-26 02:22:22 +0000

    login form_with change

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
    #@cart_items = CartItem.new
     @cart_items = CartItem.where(customer_id: current_customer.id)
